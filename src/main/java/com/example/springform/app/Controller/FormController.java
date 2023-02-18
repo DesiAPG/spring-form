2eacc1112284e588bbb1034808ee820391a9315b
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-02-17 20:47:22 -0500

    Update FormController.java

--- src/main/java/com/example/springform/app/Controller/FormController.java
+++ src/main/java/com/example/springform/app/Controller/FormController.java
@@ -1,29 +1,41 @@
 package com.example.springform.app.Controller;
 
+import com.example.springform.app.models.domain.User;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
 
 @Controller
+@SessionAttributes("user")
 public class FormController {
 
     @GetMapping("/form")
     public String form(Model model) {
+        User user = new User();
+        user.setName("Duvitas");
+        user.setSurname("Marin");
+        user.setId("123.523.615-H");
+        model.addAttribute("user", user);
         model.addAttribute("title", "Sign in");
         return "form";
     }
 
     @PostMapping("/form")
-    public String processForm(Model model,
-                              @RequestParam String username,
-                              @RequestParam String password,
-                              @RequestParam String email) {
+    public String processForm(@Valid User user, BindingResult result, Model model, SessionStatus status) {
         model.addAttribute("title", "Result of form");
-        model.addAttribute("username", username);
-        model.addAttribute("password", password);
-        model.addAttribute("email", email);
+
+        if (result.hasErrors()) {
+            return "form";
+        }
+
+        model.addAttribute("user", user);
+        status.setComplete();
         return "result";
     }
 
