70b07249972ef8f6723de60e3bf75d6f11f530e2
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-02-18 21:47:22 -0500

    Create RegexIdValidator.java

---
+++ src/main/java/com/example/springform/app/validation/RegexIdValidator.java
@@ -0,0 +1,17 @@
+package com.example.springform.app.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.lang.annotation.Annotation;
+
+public class RegexIdValidator implements ConstraintValidator<RegexIdentificator, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        if (value.matches("[0-9]")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
