6792630cc714d2c8043e2922940b77ba9c59c41d
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-03-02 12:03:14 -0500

    Create Country.java

---
+++ src/main/java/com/example/springform/app/models/domain/Country.java
@@ -0,0 +1,40 @@
+package com.example.springform.app.models.domain;
+
+public class Country {
+    private Integer id;
+    private String code;
+    private String name;
+
+    public Country() {
+    }
+
+    public Country(Integer id, String code, String name) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
