2b53ff8400bf2ff3f40a1ae723a4cf6775075ab6
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-03-03 08:53:40 -0500

    Update CountryService.java

--- src/main/java/com/example/springform/app/services/CountryService.java
+++ src/main/java/com/example/springform/app/services/CountryService.java
@@ -5,7 +5,7 @@ import com.example.springform.app.models.domain.Country;
 import java.util.List;
 
 public interface CountryService {
-    public List<Country> list();
+    public List<Country> listCountries();
 
     public Country getId(Integer id);
 }
